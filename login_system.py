95b160d1b750f309004c7ad2cd7b1047cddd66cc
Windula Dissanayake <charithwinwindula@gmail.com>
2021-11-25 00:59:05 +0530

    updates

--- login_system.py
+++ login_system.py
@@ -6,30 +6,32 @@ action = input("Enter your needed action; LOGIN = L , REGISTER = R : ")
 str_action = str(action)
 if str_action == "L":
     print("Login window opens...")
-if str_action == "R":
+elif str_action == "R":
     print("Register window opens...")
+else:
+    print("please check your input")
 
 #Login pathway
+for i in range(0,3):
+    #Enter data, email, password
+    print("Please enter your email and password")
+    old_email = input("Enter your email  : ")
+    old_password = input("Enter your password  :  ")
 
-#Enter data, email, password
-print("Please enter your email and password")
-old_email = input("Enter your email  : ")
-old_password = input("Enter your password  :  ")
+    #Database
+    stored_email = "charithwinwindula@gmail.com"
+    stored_password = "password"
 
-#Database
-stored_email = "charithwinwindula@gmail.com"
-stored_password = "password"
-
-#Compare input with data in database
-if stored_email == str(old_email) and stored_password == str(old_password):
-    print("Your entered data are valid")
-else:
-    print("check your input")
-
+    #Compare input with data in database
+    if stored_email == str(old_email) and stored_password == str(old_password):
+        print("Your entered data are valid")
+        break
+    else:
+        #Go back to -> Enter data, email, password
+        print("check your input")
 #Login permision granted
 
 #Register pathway
-
 #Enter data, name, email, password, confirm password
 print("Please enter your Name, Email, Password and Confirm Password")
 new_name = input("Enter your Name  :  ")
@@ -48,5 +50,4 @@ if new_email == old_email and new_password == old_password:
     print("This user has registered before. Please Login")
 else:
     print("Registration successful")
-
 #Save and exit
