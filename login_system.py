ba5774ceef395c13b4359b3b02806948d8092a79
Windula Dissanayake <charithwinwindula@gmail.com>
2021-11-25 22:41:12 +0530

    Updates

--- login_system.py
+++ login_system.py
@@ -1,53 +1,25 @@
-#Enter to the system
-print("------WINDULA'S LOGIN SYSTEM------")
-
 #Select an option, Login or Register
-action = input("Enter your needed action; LOGIN = L , REGISTER = R : ")
+action = input("SELECT : LOGIN = L / REGISTER = R  ")
 str_action = str(action)
+
+#Login input
 if str_action == "L":
-    print("Login window opens...")
-elif str_action == "R":
-    print("Register window opens...")
-else:
-    print("please check your input")
+    print("LOGIN")
+    old_email = input("Enter your email = ")
+    old_password = input("Enter your password = ")
 
-#Login pathway
-for i in range(0,3):
-    #Enter data, email, password
-    print("Please enter your email and password")
-    old_email = input("Enter your email  : ")
-    old_password = input("Enter your password  :  ")
+#Register input
+if str_action == "R":
+    print("REGISTER")
+    new_name = input("Enter your name = ")
+    new_email = input("Enter your email = ")
+    new_password = input("Enter a password = ")
+    new_data = "{} | {} | {}".format(new_name, new_email, new_password)
 
-    #Database
-    stored_email = "charithwinwindula@gmail.com"
-    stored_password = "password"
-
-    #Compare input with data in database
-    if stored_email == str(old_email) and stored_password == str(old_password):
-        print("Your entered data are valid")
-        break
-    else:
-        #Go back to -> Enter data, email, password
-        print("check your input")
-#Login permision granted
-
-#Register pathway
-#Enter data, name, email, password, confirm password
-print("Please enter your Name, Email, Password and Confirm Password")
-new_name = input("Enter your Name  :  ")
-new_email = input("Enter your Email  : ")
-new_password = input("Enter your password  :  ")
-new_password2 = input("Confirm your password  :  ")
-
-#Check password accuracy
-if new_password == new_password2:
-    print("Two passwords match")
-else:
-    print("Two password do not match")
-
-#Check for old records
-if new_email == old_email and new_password == old_password:
-    print("This user has registered before. Please Login")
-else:
-    print("Registration successful")
-#Save and exit
+user_data = open("user.txt", "r+")
+user_data.seek(0)
+data = user_data.read(100)
+if len(data) > 0 :
+    user_data.write("\n")
+user_data.write(new_data)
+user_data.close()
